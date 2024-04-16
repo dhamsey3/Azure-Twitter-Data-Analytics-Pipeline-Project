accf4f4404bf8f6adc82d8d03f8863e25e602f32
dhamsey3 <73405591+dhamsey3@users.noreply.github.com>
2024-04-16 03:05:34 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,3 +2,14 @@ provider "azurerm" {
   features {}
   version = "~> 2.45"  # Use an appropriate provider version
 }
+
+terraform {
+  required_version = ">= 0.14"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.45"
+    }
+  }
+}
