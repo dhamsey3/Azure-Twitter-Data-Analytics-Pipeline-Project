5c21ed7fdab95edb621b224681e0978f81dd7955
dhamsey3 <73405591+dhamsey3@users.noreply.github.com>
2024-04-16 03:10:52 +0200

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,19 +1,34 @@
 variable "resource_group_name" {
-  type    = string
   description = "Specifies the name of the resource group"
+  type        = string
 }
 
 variable "location" {
-  type    = string
   description = "Specifies the Azure Region"
+  type        = string
 }
 
 variable "storage_account_name" {
-  type    = string
   description = "Specifies the name of the storage account"
+  type        = string
 }
 
 variable "function_app_name" {
-  type    = string
   description = "Specifies the name of the function app"
+  type        = string
+}
+
+variable "snowflake_user" {
+  description = "Snowflake user name"
+  type        = string
+}
+
+variable "snowflake_password" {
+  description = "Snowflake password"
+  type        = string
+}
+
+variable "snowflake_account" {
+  description = "Snowflake account identifier"
+  type        = string
 }
