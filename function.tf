f02dde2cffc307c31b8f8ef2f95f5f05ae5931b2
dhamsey3 <73405591+dhamsey3@users.noreply.github.com>
2024-04-16 03:09:45 +0200

    Create function.tf

---
+++ function.tf
@@ -0,0 +1,44 @@
+resource "azurerm_app_service_plan" "function_app_plan" {
+  name                = "${var.function_app_name}-plan"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  kind                = "FunctionApp"
+  reserved            = true  # Required for Linux
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "twitter_processor" {
+  name                = var.function_app_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  app_service_plan_id = azurerm_app_service_plan.function_app_plan.id
+  storage_account_name= azurerm_storage_account.main.name
+  os_type             = "linux"
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME"           = "python"
+    "AzureWebJobsStorage"                = azurerm_storage_account.main.primary_connection_string
+    "FUNCTIONS_EXTENSION_VERSION"        = "~3"
+    "SNOWFLAKE_USER"                     = var.snowflake_user
+    "SNOWFLAKE_PASSWORD"                 = var.snowflake_password
+    "SNOWFLAKE_ACCOUNT"                  = var.snowflake_account
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  connection_strings {
+    name  = "SNOWFLAKE_DB_CONNECTION"
+    type  = "SQLAzure"
+    value = "Server=${var.snowflake_account};Database=<database_name>;User Id=${var.snowflake_user};Password=${var.snowflake_password};"
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
